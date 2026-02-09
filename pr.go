9181e03bc3381e483e3b9b2b4bbfcfb6e05a43a8
Ganashree18 <maheshganashree@gmail.com>
2026-02-09 15:19:02 +0530

    Updated prime check logic

--- pr.go
+++ pr.go
@@ -1,22 +1,24 @@
-// package main
-// import "fmt"
+package main
+import "fmt"
 
-// func main(){
+func main(){
 
-// 	fmt.Println("haiii")
+	fmt.Println("haiii")
+	fmt.Println("helloooo")
 
 	
 
 
-// func main() {
-//     var n int
-//     fmt.Print("Enter number of terms: ")
-//     fmt.Scan(&n)
+func main() {
+    var n int
+    fmt.Print("Enter number of terms: ")
+    fmt.Scan(&n)
 
-//     a, b := 0, 1
-//     fmt.Print("Fibonacci series: ")
-//     for i := 0; i < n; i++ {
-//         fmt.Print(a, " ")
-//         a, b = b, a+b
-//     }
-// }
+    a, b := 0, 1
+    fmt.Print("Fibonacci series: ")
+    for i := 0; i < n; i++ {
+        fmt.Print(a, " ")
+        a, b = b, a+b
+    }
+}
+}
